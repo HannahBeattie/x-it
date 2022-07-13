5f5383c5ef2fbfc80678bde52bcfa1b2f4c92d25
Hannah Beattie <hannahdbeattie@gmail.com>
2022-07-13 12:22:48 +1200

    addon: Add 'cancel' button to event UI

--- googleAddOn/src/eventUI.js
+++ googleAddOn/src/eventUI.js
@@ -3,34 +3,34 @@ function onEventOpen(evt) {
     Logger.log('No event!')
     return
   }
-  Logger.log('Event opened: ' + JSON.stringify({ evt }))
   const calEvt = Calendar.Events.get(evt.calendar.calendarId, evt.calendar.id)
-  Logger.log("Got calendar event? " + JSON.stringify(calEvt))
+  if (!calEvt) {
+    Logger.log('Unable to find calendar event for ' + JSON.stringify(evt))
+    return
+  }
+  Logger.log('Got calendar event: ' + JSON.stringify(calEvt))
 
   const builder = CardService.newCardBuilder()
   const sect = CardService.newCardSection()
   sect.addWidget(
-    CardService.newDecoratedText()
-      .setTopLabel(calEvt?.summary || ':(')
-      .setText('be antisocial')
+    CardService.newDecoratedText().setText('Secretly cancel, sneak sneak')
+  )
+  sect.addWidget(
+    CardService.newTextButton()
+      .setText('Cancel')
+      .setTextButtonStyle(CardService.TextButtonStyle.FILLED)
+      .setOnClickAction(CardService.newAction().setFunctionName('xit'))
   )
   builder.addSection(sect)
   return builder.build()
 }
 
-function onEventUpdated(evt) {
-  if (!evt) {
-    Logger.log('No event!')
-  } else {
-    Logger.log('Event opened: ' + JSON.stringify({ evt }))
-  }
-  const builder = CardService.newCardBuilder()
-  const sect = CardService.newCardSection()
-  sect.addWidget(
-    CardService.newDecoratedText()
-      .setTopLabel(evt?.summary || ':(')
-      .setText('be antisocial')
-  )
-  builder.addSection(sect)
-  return builder.build()
+function xit() {
+  return CardService.newActionResponseBuilder()
+    .setNotification(
+      CardService.newNotification().setText(
+        'You have secretly indicated your intention to cancel'
+      )
+    )
+    .build()
 }

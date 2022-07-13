5ccbe9cdd6061a25b3be6d0d0b70408071c2d578
Hannah Beattie <hannahdbeattie@gmail.com>
2022-07-13 12:38:44 +1200

    next: create server to be used by backend

--- next/pages/api/x-it.js
+++ next/pages/api/x-it.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// const db = {}
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const evtInfo = req.body
+  console.log('Got x-it request:', evtInfo)
+  res.status(200).json(evtInfo)
 }

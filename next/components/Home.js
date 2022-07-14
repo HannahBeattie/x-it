c2d29a9d9321dac0ca690abe573b5663693ce224
Hannah Beattie <hannahdbeattie@gmail.com>
2022-07-14 18:58:50 +1200

    logo

--- next/components/Home.js
+++ next/components/Home.js
@@ -1,9 +1,10 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Box, Button, Image } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
       <Box bg="#1A202C" w="100%" p={6} color="white">
+        <Image src="https://x-it.vercel.app/xit-120.png" alt="will go here" />
         Welcome
       </Box>
     </>

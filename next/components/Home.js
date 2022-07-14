9da38112e9fdf46c745e75c40ee0a63ebb83015b
Hannah Beattie <hannahdbeattie@gmail.com>
2022-07-14 19:06:32 +1200

    logos

--- next/components/Home.js
+++ next/components/Home.js
@@ -3,9 +3,14 @@ import { Box, Button, Image } from '@chakra-ui/react'
 export default function Home() {
   return (
     <>
-      <Box bg="#1A202C" w="100%" p={6} color="white">
-        <Image src="https://x-it.vercel.app/xit-120.png" alt="will go here" />
-        Welcome
+      <Box bg="#1A202C" w="100%" color="white">
+        <Image
+          h={14}
+          padding={3}
+          marginLeft={3}
+          src="https://x-it.vercel.app/x-it-v2.png"
+          alt="will go here"
+        />
       </Box>
     </>
   )
